0cc18baf8d0a70285f42cd2d6827d6b758512c03
Tallulahwalz <tmwalznyc@gmail.com>
2019-02-16 17:30:01 +0000

    Did the assignment

--- problemSetDay2.py
+++ problemSetDay2.py
@@ -1,1 +1,19 @@
 # file name: problemSetDay2.py
+'''name = input("What is your name?")
+print(name)
+birthday = input("When is your birthday?")
+print (birthday)
+grade = input("What grade are you in?")
+print(grade)
+school = input("What school do you attend")
+print(school)
+favorite = input("What is your favorite subject?")
+print(favorite)
+grow = input("What do you want to do when you grow up?")
+print(grow)'''
+
+b = int(input("What is the value of b?"))
+a = int(input("What is the value of a?"))
+c = int(input("What is the value of c?"))
+x = ("[ -b  +/-  sqrt( b^2 - 4ac ) ]  /  2a")
+print(x)
